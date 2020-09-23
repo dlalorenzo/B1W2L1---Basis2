1c5c47ace18c1618461827cbc36d0c77492ad43d
lorenzo <99051451@mydavinci.nl>
2020-09-23 11:26:30 +0200

    .

---
+++ JavaScript/basisLogic.js
@@ -0,0 +1,17 @@
+var name = prompt("hello, what is your name");
+var surname = prompt("and your surname?");
+var age = prompt("and your age?");
+//var that ask for name/surname/age
+
+   document.write("welcome " + name + "<br>"+ " surname: " + surname + "<br>"+ " age: ");
+   // shows the name/surname/age on screen
+
+   if (age > 17) {
+   	alert("welcome matie")
+   	document.write(age)
+   }else{
+   	alert("goodbye boy")
+   	document.write(age)
+   };
+
+
